42bd7090e849b58fd71120c7489bb41c2046e53a
Nicolas Pieter <nicolaspieter@yahoo.com>
2017-03-07 00:34:10 +0800

    added checkout success

--- user/checkout-success.php
+++ user/checkout-success.php
@@ -1,24 +1,28 @@
-<?php include __DIR__ . '/include/header.php'; ?>
+<?php include __DIR__ . '/include/header-home.php'; ?>
 
-<body class="hold-transition skin-black layout-top-nav">
-<div class="wrapper">
-  
-  <?php include __DIR__ . '/include/dash-navigation.php'; ?>
-
-<!-- Full Width Column -->
-  <div class="content-wrapper" id="app">
-    <div class="container">
-      <!-- Main content -->
-      <section class="content col-sm-9">
-      	asdf
-      </section>
-      <!-- /.content -->
-    </div>
-    <!-- /.container -->
-  </div>
-  <!-- /.content-wrapper -->
-</div>
-<!-- ./wrapper -->
+<body>
+	<div class="background-image-left hidden-xs" style="background-image: url(&quot;../dist/img/Chef-sign-up-bg.jpg&quot;);"></div>
 
 
-<?php include __DIR__ . '/include/footer.php'; ?>
+	<div class="container navigation">
+		<a href="#" class="logo">
+			<img src="../dist/img/Logo.png" height="38px">
+		</a>
+	</div>
+
+
+	<div class="container">
+		<div class="row col-sm-6 col-sm-offset-6 signup">
+			<h1 class="text-capitalize">
+				Payment successful
+			</h1>
+			<h4>
+				Your payment went successfully. You can manage your chef bookings in your profile
+			</h4>
+			<br>
+			<a href="request-all.php" class="btn btn-temp">Go to my bookings</a>
+			<a href="#" class="btn">Back home</a>
+		</div>
+
+	</div>
+	<?php include __DIR__ . '/include/footer.php'; ?>
