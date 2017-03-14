8562e117cbf23a7e71dde4a038546557f1bce600
Nicolas Pieter <nicolaspieter@yahoo.com>
2017-03-14 12:12:20 +0800

    added book a chef homepage

---
+++ user/index-book_a_chef.php
@@ -0,0 +1,210 @@
+
+<?php include __DIR__ . '/include/header.php'; ?>
+
+<body class="hold-transition skin-black layout-top-nav">
+  <div class="wrapper">
+      <div class="main-area" style="background-image: url(&quot;../dist/img/homepage--for-chefs.jpg&quot;);">
+        <!-- Navigation -->
+        <header class="main-header main-header-home">
+          <nav class="navbar">
+            <div class="container">
+              <a class="navbar-brand" href="index.html">
+                <img src="../dist/img/Logo.png" height="35px">
+                <div class="mini-logo text-capitalize">
+                  chef-on-demand
+                </div>
+              </a>
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                <span class="glyphicon glyphicon-th-list"></span>                      
+              </button>
+              <div class="collapse navbar-collapse row text-uppercase" id="navbar">
+                <!-- Navigation left -->
+                <div class="navbar-left">
+                  <ul class="nav navbar-nav">
+                    <li>
+                      <a href="#">chefs</a>
+                    </li>
+                    <li>
+                      <a href="#">how it works</a>
+                    </li>
+                  </ul>
+                </div>
+                <!-- Navigation left end -->
+                <!-- Navigation right -->
+                <ul class="nav navbar-nav navbar-right">
+                  <li>
+                    <a href="#">log in</a>
+                  </li>
+                  <li>
+                    <button href="#" class="btn btn-default text-uppercase">Become Chef</button>
+                  </li>
+                </ul>
+                <!-- Navigation right (end) -->
+                &nbsp;
+              </div>
+            </div>
+          </nav>
+        </header>
+        <!-- Navigation (end) -->
+        <div class="text-uppercase heading">
+          <div class="row col-sm-8 col-sm-offset-2">
+            <h1>Best Chefs-on-demand<br class="hidden-xs">
+            <span class="primary-color"><b>Right near you</b></span></h1>
+            <div class="col-sm-4 col-sm-offset-4"><a href="signup.php" class="btn btn-default btn-block">Book a chef</a></div>
+          </div>
+        </div>
+        <div style="clear: both;"></div>
+      </div>
+      
+
+      <!-- Main content -->
+      <!-- 1st content row -->
+      <section class="container-fluid content chefs">
+        <div class="row content-heading">
+          <h1 class="text-capitalize">
+            Book a chef in your area
+          </h1>
+          <p class="col-sm-8 col-sm-offset-2 text-capitalize">
+            we promote and drive your sales for chef's culinary service / your pop up events / your unique spaces for corporate bookings / food tickets for you f &amp; b promo campaign
+          </p>
+        </div>
+
+        <div class="row profile-row">
+          <div class="col-sm-3">
+            <div class="box box-menu">
+              <img src="../dist/img/chef-menu-1.jpg" style="width: 100%; height: 125px;">
+              <div class="view-menu">
+                <h5>Bess Ball</h5>
+                <h6>North Quintonchester</h6>
+                <p class="view-menu-content">Thrill Friends And Family With A Make It Yourself Pizza Party</p>
+                <p>
+                  <i class="fa fa-usd"></i><span class="view-menu-price">96+</span><br class="hidden-xs">
+                  per person
+                </p>
+                <div>
+                  <button class="btn btn-flat inverse">Caribbean</button>
+                  <button class="btn btn-flat inverse">European</button>
+                  <span class="text-muted"><small>+3 more</small></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="box box-menu">
+              <img src="../dist/img/chef-menu-1.jpg" style="width: 100%; height: 125px;">
+              <div class="view-menu">
+                <h5>Carolyn Sanders</h5>
+                <h6>Grayceshire</h6>
+                <p class="view-menu-content">Delivering kisses and miracles</p>
+                <p>
+                  <i class="fa fa-usd"></i><span class="view-menu-price">57+</span><br class="hidden-xs">
+                  per person
+                </p>
+                <div>
+                  <button class="btn btn-flat inverse">International</button>
+                  <button class="btn btn-flat inverse">Spanish</button>
+                  <span class="text-muted"><small>+2 more</small></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="box box-menu">
+              <img src="../dist/img/chef-menu-1.jpg" style="width: 100%; height: 125px;">
+              <div class="view-menu">
+                <h5>Steven Marsh</h5>
+                <h6>Jamirchester</h6>
+                <p class="view-menu-content">When to use high heat on your barbeque</p>
+                <p>
+                  <i class="fa fa-usd"></i><span class="view-menu-price">127+</span><br class="hidden-xs">
+                  per person
+                </p>
+                <div>
+                  <button class="btn btn-flat inverse">Caribbean</button>
+                  <button class="btn btn-flat inverse">European</button>
+                  <span class="text-muted"><small>+3 more</small></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="box box-menu">
+              <img src="../dist/img/chef-menu-1.jpg" style="width: 100%; height: 125px;">
+              <div class="view-menu">
+                <h5>Harriet Diaz</h5>
+                <h6>Feilbury</h6>
+                <p class="view-menu-content">Bbq grilling versus the great debate</p>
+                <p>
+                  <i class="fa fa-usd"></i><span class="view-menu-price">77+</span><br class="hidden-xs">
+                  per person
+                </p>
+                <div>
+                  <button class="btn btn-flat inverse">Caribbean</button>
+                  <button class="btn btn-flat inverse">European</button>
+                  <span class="text-muted"><small>+3 more</small></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="profile-all">
+            <a href="#" class="btn btn-default text-uppercase">browse all chefs</a>
+          </div>
+        </div>
+      </section>
+      <!-- 1st content row (end) -->
+
+      <!-- 2nd content row -->
+      <div class="container-fluid content chefs-2">
+        <div class="row content-heading">
+          <h1 class="text-capitalize">
+            How to book a chef
+          </h1>
+          <p>
+            When it comes to barbequing, there are two main schools of thought for the techniques that you can use.
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4 how">
+            <img class="how-pic" src="../dist/img/content.png" width="100px">
+            <div class="how-heading text-capitalize">
+              create request
+            </div>
+            <div class="how-quote">
+              You may be a person who loves to cook. You absolutely love to spend time in your kitchen.
+            </div>
+          </div>
+          <div class="col-sm-4 how">
+            <img class="how-pic" src="../dist/img/auction.png" width="100px">
+            <div class="how-heading text-capitalize">
+              get bids
+            </div>
+            <div class="how-quote">
+              You may be a person who loves to cook. You absolutely love to spend time in your kitchen.
+            </div>
+          </div>
+          <div class="col-sm-4 how">
+            <img class="how-pic" src="../dist/img/chef.png" width="100px">
+            <div class="how-heading text-capitalize">
+              choose chef
+            </div>
+            <div class="how-quote">
+              You may be a person who loves to cook. You absolutely love to spend time in your kitchen.
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 2nd content row (end) -->
+      <!-- Main content (end) -->
+
+      <!-- Footer -->
+      
+        <?php include __DIR__ . '/include/footer-home.php'; ?>
+      
+      <!-- Footer (end) --> 
+  
+  </div>
+  <!-- /.content-wrapper -->
+</div>
+<!-- ./wrapper -->
+<?php include __DIR__ . '/include/footer.php'; ?>
