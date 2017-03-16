65f1bdd83afbda85a5d739bba32033a9425f1b56
Nicolas Pieter <nicolaspieter@yahoo.com>
2017-03-16 15:47:15 +0800

    chef booking done

--- user/chef-book.php
+++ user/chef-book.php
@@ -51,8 +51,211 @@
     <div class="container">
       <!-- Main content -->
         <section class="content">
-          <div>
-            <img src="../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" height="60px">
+          <div class="row">
+            <div class="col-xs-3 col-sm-1">
+              <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" height="60px">
+            </div>
+            <div class="col-xs-3">
+              <h4>Richard Hunt</h4>
+              <span class="h6">Katelyntown</span>
+            </div>
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-lg-7">
+              <div class="box box-menu box-padded">
+                <div class="row">
+                  <div class="col-sm-3">
+                    <img src="../../dist/img/chef-menu-2.jpg" class="img-circle" height="120px" width="120px">
+                  </div>
+                  <div class="col-sm-9">
+                    <span class="h2">Dishes Friday</span>
+                    <p><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus provident, facere nisi labore dolore, similique obcaecati cum, totam autem voluptates dignissimos neque distinctio dolores ut reprehenderit numquam deserunt illum aliquam.</p>
+                  </div>
+                  <div class="col-xs-3 col-sm-offset-3">
+                    <h5>Available dates</h5>
+                    <label>Every Friday</label>
+                  </div>
+                  <div class="col-xs-3 col-xs-offset-1">
+                    <h5>Cuisine</h5>
+                    <label>Italian</label>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Appetizer menu option -->
+              <div class="row">
+                <div class="col-sm-12">
+                  <label>Appetizer options</label>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        Keeping that cooking area clean
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div> 
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        The perfect pot for every preparation
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        The importance of food elements
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Appetizer menu option end -->
+              <!-- Main menu option -->
+              <div class="row">
+                <div class="col-sm-12">
+                  <label>Main options</label>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        Keeping that cooking area clean
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div> 
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        The perfect pot for every preparation
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        The importance of food elements
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        Keeping that cooking area clean
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div> 
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="box box-menu inverse">
+                    <img src="../../dist/img/chef-menu-2.jpg" width="100%" height="125px">
+                    <div class="view-menu">
+                      <h5>
+                        The perfect pot for every preparation
+                      </h5>
+                      <span class="h6">
+                        There are very few appliances in my kitchen that can compete with my
+                      </span class="h6">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Main menu option end -->
+            </div>
+            <!-- left side bar end -->
+            <!-- right side bar -->
+            <div class="col-lg-5">
+              <div class="box box-menu box-padded inverse">
+                <div class="row">
+                  <div class="col-xs-12">
+                    <span class="h3">
+                      <sup><i class="fa fa-usd"></i></sup>
+                      117
+                      <sub><small>lowest price per person</small></sub>
+                    </span>
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                  <div class="col-xs-5 form-group">
+                    <label>Group size</label>
+                    <div class="input-group">
+                      <input type="number" class="form-control" placeholder="# of people" min="0">
+                      <div class="input-group-addon">
+                        <i class="fa fa-users"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-xs-7 form-group">
+                    <label>Date and time</label>
+                    <div class="input-group">
+                      <input type="text" class="form-control" id="datepicker" placeholder="Choose date">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <label>Total</label>
+                  <span class="h3 pull-right">
+                    <i class="fa fa-usd"></i>
+                    772.2
+                  </span>
+                  &nbsp;
+                </div>
+                <br>
+                <div>
+                  <a href="#" class="btn btn-default">Customize dishes &amp; pay</a>
+                </div>
+              </div>
+              <div>
+                <a href="#" class="h4 text-bold btn-block"><i class="fa fa-heart-o"></i> &nbsp; Add to my favourites</a>
+                <a href="#" class="h4 text-bold btn-block"><i class="fa fa-envelope-o"></i> &nbsp; Email</a>
+                <a href="#" class="h4 text-bold btn-block"><i class="fa fa-print"></i> &nbsp; Print</a>
+              </div>
+              <!-- right side bar end -->
+            </div>
           </div>
         </section>
       <!-- /.content -->
